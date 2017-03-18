7747a41e4ac09f256b5244d105976601bd437c55
ryanbmarx <ryanbmarx@gmail.com>
2017-03-18 17:42:18 -0500

    Rejigger child theme meta script
    To make the code more portable, the script takes a second argument, which is intended to be a text file with the necessary WP meta data.

--- node_scripts/appendChildThemeMeta.js
+++ node_scripts/appendChildThemeMeta.js
@@ -1,26 +1,31 @@
-const fs = require('fs'),
+var fs = require('fs'),
 	filename = process.argv[2],
-	childThemeString = `/* Theme Name:   Divi Child theme for Media Salad  
-	/* Theme URI:   */  
-	/* Description:  Divi 
-	/* Child Theme Author: Media Salad */
-	/* Author URI: http://www.mediasalad.com    */
-	/* Template:     Divi  */
-	/* Version:      1.0.0  */
-	/* License:      GNU General Public License v2 or later */
-	/* License URI:  http://www.gnu.org/licenses/gpl-2.0.html 
-	/* Tags:          */
-	/* Text Domain:  MSDivi */ \n`;
+	childMeta = process.argv[3]
+	// childThemeString = `/* Theme Name:   Divi Child theme for Media Salad  
+	// /* Theme URI:   */  
+	// /* Description:  Divi 
+	// /* Child Theme Author: Media Salad */
+	// /* Author URI: http://www.mediasalad.com    */
+	// /* Template:     Divi  */
+	// /* Version:      1.0.0  */
+	// /* License:      GNU General Public License v2 or later */
+	// /* License URI:  http://www.gnu.org/licenses/gpl-2.0.html 
+	// /* Tags:          */
+	// /* Text Domain:  MSDivi */ \n`;
 
 fs.readFile(`${filename}`, 'utf8', (err, data) => {
 	if (err) throw err;
-	const outputData = childThemeString + data;
 
-	fs.writeFile(`${filename}`, outputData, (err) => {
-		if(err) {
-			return console.log(err);
-		} else {
-			return console.log('done appending child theme metadata');
-		}
+	fs.readFile(childMeta, 'utf8', (err, childThemeString) => {
+
+		var outputData = childThemeString + data;
+
+		fs.writeFile(`${filename}`, outputData, (err) => {
+			if(err) {
+				return console.log(err);
+			} else {
+				return console.log('done appending child theme metadata');
+			}
+		}); 
 	}); 
 });	
